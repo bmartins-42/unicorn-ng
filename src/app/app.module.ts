185ad2b7d05f86d90a9a615190745ee5e86d3d6f
Benoit Martins <benoit.martins@outlook.com>
2021-07-26 13:48:35 +0200

    :sparkles: Add new component to display unicorn count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UnicornListComponent } from './pages/unicorn-list/unicorn-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnicornListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/unicorn-list/unicorn-list.component.ts
@@ -0,0 +1,28 @@
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UnicornService } from 'src/app/shared/services/unicorn.service';
+import { Unicorn } from 'src/app/shared/models/unicorn.model';
+
+@Component({
+  selector: 'app-unicorn-list',
+  templateUrl: './unicorn-list.component.html',
+  styleUrls: ['./unicorn-list.component.scss']
+})
+export class UnicornListComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+
+  public unicorns$: Unicorn[] = []
+
+  constructor(private unicornService: UnicornService) { }
+
+  ngOnInit(): void {
+    this.unicornService.getUnicorn()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(unicorns => this.unicorns$ = unicorns)
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next()
+  }
+}
